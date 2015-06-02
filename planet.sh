c65300f49509caabfb9bdaa02bdb2ef65ed5a415
Baron Schwartz <baron@xaprb.com>
2015-06-02 15:13:00 -0400

    remove git ssh var

--- planet.sh
+++ planet.sh
@@ -5,15 +5,15 @@ set -e
 if [ ! -e ~/repos/planetmongo-src ]; then
 	mkdir -p ~/repos
 	cd ~/repos
-	GIT_SSH_COMMAND='git -i ~/.ssh/id_rsa-pm' git clone git@github.com:xaprb/planetmongo-src
+	git clone git@github.com:xaprb/planetmongo-src
 	cp -R planetmongo-src planetmongo-web
 fi
 cd ~/repos/planetmongo-web
 git checkout gh-pages
 cd ~/repos/planetmongo-src
-GIT_SSH_COMMAND='git -i ~/.ssh/id_rsa-pm' git pull
+git pull
 ./planet.py config.ini
 cd ~/repos/planetmongo-web
 git add --all .
 git commit -am "generate $(date)"
-GIT_SSH_COMMAND='git -i ~/.ssh/id_rsa-pm' git push
+git push
