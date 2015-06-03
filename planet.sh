0ba3ddac6d0eb14937ae41a29535411ac04a8d64
Baron Schwartz <baron@xaprb.com>
2015-06-02 21:28:56 -0400

    s/mongo/redis/g

--- planet.sh
+++ planet.sh
@@ -3,21 +3,21 @@
 set -e
 
 eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa-pm
+ssh-add ~/.ssh/id_rsa-pr
 ssh-add -l
-if [ ! -e ~/repos/planetmongo-src ]; then
+if [ ! -e ~/repos/planetredis-src ]; then
 	mkdir -p ~/repos
 	cd ~/repos
-	git clone git@github.com:xaprb/planetmongo-src
-	cp -R planetmongo-src planetmongo-web
+	git clone git@github.com:xaprb/planetredis-src
+	cp -R planetredis-src planetredis-web
 fi
-cd ~/repos/planetmongo-web
+cd ~/repos/planetredis-web
 git checkout gh-pages
 git pull
-cd ~/repos/planetmongo-src
+cd ~/repos/planetredis-src
 git pull
 ./planet.py config.ini
-cd ~/repos/planetmongo-web
+cd ~/repos/planetredis-web
 git add --all .
 git commit -am "generate $(date)"
 git push
